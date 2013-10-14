b39c6af75659c4888bd341c994f70f9f726f2c29
Anton Kuznetsov <isnifer@gmail.com>
2013-10-14 07:30:09 +0400

    Добавлена разметка и логика для страниц "Карточка лектора" и "Список лекторов"

--- js/myApp.js
+++ js/myApp.js
@@ -6,18 +6,32 @@ myApp.config(function ($routeProvider) {
 
         // Страница школы
         .when('/', {
-            templateUrl: '../templates/school.html',
+            templateUrl: 'templates/school.html',
             controller: 'School'
         })
 
+        // Список студентов
         .when('/students/', {
-            templateUrl: '../templates/students.html',
+            templateUrl: 'templates/students.html',
             controller: 'Students'
         })
 
-        .when('/profile/:id', {
-            templateUrl: '../templates/student_card.html',
+        // Карточка студента
+        .when('/student/:id', {
+            templateUrl: 'templates/student_card.html',
             controller: 'Students'
+        })
+
+        // Список лекторов
+        .when('/lecturers/', {
+            templateUrl: 'templates/lecturers.html',
+            controller: 'Lecturers'
+        })
+
+        // Карточка лектора
+        .when('/lecturer/:id', {
+            templateUrl: 'templates/lecturer_card.html',
+            controller: 'Lecturers'
         });
 
 });
@@ -30,7 +44,7 @@ myApp.controller('School', function ($scope, $http) {
 
 myApp.controller('Students', function ($scope, $http, $routeParams) {
 
-    $scope.title = 'Студенты',
+    $scope.title = 'Студенты';
     $scope.students = [];
 
     var callback = function (data) {
@@ -41,14 +55,71 @@ myApp.controller('Students', function ($scope, $http, $routeParams) {
     };
 
     $http
-        .get('students.json')
+        .get('data.json')
         .success(function (data) {
-            $scope.students = data;
+            $scope.students = data.students;
             callback($scope.students);
-            console.log($scope.students);
         })
         .error(function (error) {
             console.log(error);
         });
+
+});
+
+myApp.controller('Lecturers', function ($scope, $http, $routeParams) {
+
+    // Заголовок страницы
+    $scope.title = 'Лекторы';
+
+    $scope.lecturers = [];
+
+    var callback = function (data) {
+        var j = 0,
+            lecturersLength = data.length,
+            lecturerId = parseInt($routeParams.id);
+        
+        // Если приходят данные с ID лектора
+        if ($routeParams.id) {
+            // Определяем, что это за лектора
+            $scope.lecturer = (function () {
+                for (j; j < lecturersLength; j++) {
+                    if (data[j].id === lecturerId) {
+                        return data[j];
+                    }
+                }
+            }());
+            // В качестве заголовка его ФИО
+            $scope.title = $scope.lecturer.name;
+        }
+    };
+
+    $http
+        .get('data.json')
+        .success(function (data) {
+            $scope.lecturers = data.lecturers;
+            callback($scope.lecturers);
+        })
+        .error(function (error) {
+            console.log(error);
+        });
+
+});
+
+/* Переключалка списка всех лекций автора */
+myApp.directive('viewAllLectures', function () {
+
+    return {
+        restrict: 'C',
+        link: function (scope, elem, attrs) {
+            elem[0].addEventListener('click', function(e){
+                var popUp = document.getElementsByClassName('b-lecturer__list_all')[0];
+                if (popUp.classList.contains('b-lecturer__list_state_hidden')) {
+                    popUp.classList.remove('b-lecturer__list_state_hidden');
+                } else {
+                    popUp.classList.add('b-lecturer__list_state_hidden');
+                }
+            }, false);
+        }
+    };
 
 });
